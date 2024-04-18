b7d697d89e345654b4b73771f5744a2897d034a7
olee <olee.techs@gmail.com>
2024-04-18 13:12:10 +0600

    add admin panel quistion and answere

--- Backend/mychatbot/chat/bot.py
+++ Backend/mychatbot/chat/bot.py
@@ -1,16 +1,25 @@
 import nltk
 nltk.download('punkt')
 from nltk.chat.util import Chat, reflections
+from .models import QA_Pair  
 
-pairs = [
-    [r"hi|hello", ["Hello!", "Hi there!"]],
-    [r"how are you?", ["I'm good, how are you?"]],
-    [r"quit", ["Bye, have a good day!"]],
-    [r"are you give hosting|are you give deploy", ["Yes"]],
-    [r"kreatech|what is kreatech|know kreatech\?*|kreatech\?", ["I know about Kreatech. It is a leading software company"]],
-    [r"কে আপনি ? | আপনি কে ? ", ["আমি মানুষ "]]
-]
+def load_pairs():
+    db_pairs = [
+        [fr"{pair.question}", [pair.answer]]
+        for pair in QA_Pair.objects.all()
+    ]
+    static_pairs = [
+        [r"hi|hello", ["Hello!", "Hi there!"]],
+        [r"who made you ?| your creator ? |who make you", ["I am developed by Olee Ahmmed (Backend Developer)"]],
+        [r"how are you?", ["I'm good, how are you?"]],
+        [r"quit", ["Bye, have a good day!"]],
+        [r"are you give hosting|are you give deploy", ["Yes"]],
+        [r"kreatech|what is kreatech|know kreatech\?*|kreatech\?", ["I know about Kreatech. It is a leading software company"]],
+        [r"কে আপনি ? | আপনি কে ?", ["আমি মানুষ "]]
+    ]
+    return static_pairs + db_pairs
 
+pairs = load_pairs()
 chatbot = Chat(pairs, reflections)
 
 def chat(user_input):
--- Backend/mychatbot/chat/models.py
+++ Backend/mychatbot/chat/models.py
@@ -6,3 +6,12 @@ class Message(models.Model):
 
     def __str__(self):
         return f"Question: {self.text[:50]}..."  # Shows the beginning of the text
+    
+    
+class QA_Pair(models.Model):
+    question = models.CharField(max_length=1024)
+    answer = models.CharField(max_length=1024)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Question: {self.question[:50]}... - Answer: {self.answer[:50]}"
